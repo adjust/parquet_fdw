1a3ddd83daecea37dd3e55288153ee8544bd2b57
Ildar Musin <zildermann@gmail.com>
2021-07-05 18:05:39 +0200

    Update install_arrow.sh script for Github Actions

--- install_arrow.sh
+++ install_arrow.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-apt install -y -V apt-transport-https lsb-release ca-certificates
-wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
-tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
-deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
-APT_LINE
-apt update --allow-insecure-repositories
-apt install -y -V --allow-unauthenticated libarrow-dev libparquet-dev
+apt update
+apt install -y -V ca-certificates lsb-release wget
+wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+apt update
+apt install -y -V libarrow-dev libparquet-dev
